e61b96d22b22536f2db3b23e20845c3a23991678
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2020-07-15 18:20:26 +0200

    chore: update Sentry files

--- evan/site/static/js/sentry.js
+++ evan/site/static/js/sentry.js
@@ -1,14 +1,14 @@
 Sentry.init({
-    dsn: 'https://5b2d2f25df16403cb489cab3213ec892@sentry.io/1426084',
-    release: GIT_REV,
-    environment: 'production',
-    integrations: [new Sentry.Integrations.Vue()],
+  dsn: 'https://5b2d2f25df16403cb489cab3213ec892@o124046.ingest.sentry.io/1426084',
+  release: GIT_REV,
+  environment: 'production',
+  integrations: [new Sentry.Integrations.Vue({tracing: false})],
 });
 
 if (USER_ID > 0) {
-    Sentry.configureScope(function (scope) {
-        scope.setUser({
-            id: USER_ID
-        });
+  Sentry.configureScope(function (scope) {
+    scope.setUser({
+      id: USER_ID
     });
+  });
 }
