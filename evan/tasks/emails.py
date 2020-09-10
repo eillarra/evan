423b7f66ad66cebc1def8297ad9a8f4a17b4ae86
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2020-09-10 10:29:23 +0200

    fix(tasks): use a rate limit when sending emails

--- evan/tasks/emails.py
+++ evan/tasks/emails.py
@@ -4,7 +4,7 @@ from typing import List
 from evan.tools.mailer import send_email
 
 
-@task()
+@task(rate_limit="2/m")
 def send_template_email(template: str, subject: str, from_email: str, to: List[str], context_data: dict = {}):
     send_email(
         template=template, subject=subject, from_email=from_email, to=to, context_data=context_data,
