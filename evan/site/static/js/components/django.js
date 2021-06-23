96ca87440b0c6f3de861ccd381381296c55ed00e
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2021-06-23 23:30:13 +0200

    chore: improve django+vue forms

--- evan/site/static/js/components/django.js
+++ evan/site/static/js/components/django.js
@@ -3,22 +3,48 @@ var DjangoComponents = {
   'django-form': {
     data: function () {
       return {
+        csrfToken: document.querySelector('html').dataset.csrfToken,
         mutable: {}
       };
     },
     props: {
+      action: {
+        type: String,
+        required: true
+      },
       fields: {
         type: Array,
         default: function () {
           return [];
         }
+      },
+      btnText: {
+        type: String,
+        default: 'Send'
       }
     },
     template: `
-      <div class="row q-col-gutter-sm">
-        <q-input dense filled v-for="f in fields" :name="f.name" :label="f.label" :type="f.type" :required="f.required" class="col-12" :class="f.class" v-model="mutable[f.name]" />
-      </div>
-    `
+      <form method="post" :action="action">
+        <input type="hidden" name="csrfmiddlewaretoken" :value="csrfToken">
+        <div class="row q-col-gutter-sm">
+          <q-input dense filled v-for="f in fields" :name="f.name" :label="f.label + ((f.required) ? ' *' : '')" :type="f.type" :required="f.required" class="col-12" :class="f.class" v-model="mutable[f.name]" />
+        </div>
+        <slot class="q-mt-md"></slot>
+        <q-btn unelevated color="primary" type="submit" class="q-mt-lg" :disable="!valid">{{ btnText }}</q-btn>
+      </form>
+    `,
+    computed: {
+      valid: function () {
+        var mutable = this.mutable;
+        var fails = 0;
+
+        _.each(this.fields, function (f) {
+          if (f.required && (mutable[f.name] == undefined || !mutable[f.name].length)) fails++;
+        });
+
+        return fails == 0;
+      }
+    }
   }
 
 };
