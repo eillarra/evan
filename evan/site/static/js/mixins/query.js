f91bbe4eba82764965a597690156bc6a4eff3b0c
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2021-07-09 09:15:25 +0200

    style: make ui less dense for sm screens

--- evan/site/static/js/mixins/query.js
+++ evan/site/static/js/mixins/query.js
@@ -5,6 +5,11 @@ var QueryMixin = {
       query: this.q || ''
     }
   },
+  methods: {
+    customFilter: function (rows, terms, cols, getCellValue) {
+      return Evan.utils.filter(rows, terms);
+    }
+  },
   watch: {
     query: function (val, oldVal) {
       if (val != '') this.$router.replace({query: {q: val}});
