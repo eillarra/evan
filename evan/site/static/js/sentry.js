3b024e4bc37aba5114303eabc8f15f5d7415eb8b
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2021-08-19 17:27:42 +0200

    chore: enable Sentry BrowserTracing

--- evan/site/static/js/sentry.js
+++ evan/site/static/js/sentry.js
@@ -3,9 +3,13 @@ var DJANGO_VARS = document.querySelector('html').dataset;
 
 Sentry.init({
   dsn: 'https://5b2d2f25df16403cb489cab3213ec892@o124046.ingest.sentry.io/1426084',
-  release: DJANGO_VARS.gitRev,
+  release: DJANGO_VARS.rev,
   environment: 'production',
-  integrations: [new Sentry.Integrations.Vue({tracing: false})],
+  integrations: [
+    new Sentry.Integrations.BrowserTracing(),
+    new Sentry.Integrations.Vue()
+  ],
+  tracesSampleRate: 0.1
 });
 
 if (+DJANGO_VARS.user > 0) {
