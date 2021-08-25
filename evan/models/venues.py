916a43c04e8d6e99b352f65c65357c2a7dd12907
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2021-08-25 13:26:48 +0200

    chore: extra venue info

--- evan/models/venues.py
+++ evan/models/venues.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from evan.services.geocoding import geocode
-
 
 class Venue(models.Model):
     event = models.ForeignKey("evan.Event", related_name="venues", on_delete=models.CASCADE)
@@ -9,9 +7,7 @@ class Venue(models.Model):
     name = models.CharField(max_length=160)
     city = models.CharField(max_length=160, null=True, blank=True)
     presentation = models.TextField(null=True, blank=True)
-    address = models.CharField(max_length=160, null=True, blank=True)
-    lat = models.FloatField("Latitude", blank=True, null=True)
-    lng = models.FloatField("Longitude", blank=True, null=True)
+    gmaps = models.URLField(null=True, blank=True)
     website = models.URLField(null=True, blank=True)
 
     def __str__(self) -> str:
@@ -19,13 +15,7 @@ class Venue(models.Model):
 
     def save(self, *args, **kwargs) -> None:
         self.city = self.event.city if not self.city else self.city
-        if self.address and self.address != "":
-            self.lat, self.lng = geocode(self.full_address)
         super().save(*args, **kwargs)
-
-    @property
-    def full_address(self) -> str:
-        return "{0}, {1}, {2}".format(self.address, self.city, self.event.country)
 
 
 class Room(models.Model):
