44d57754161a85160ef9ba8f155943fc1a920a67
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2022-07-02 10:56:48 +0200

    chore: tweaks

--- evan/site/views/abstracts.py
+++ evan/site/views/abstracts.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views import generic
 
