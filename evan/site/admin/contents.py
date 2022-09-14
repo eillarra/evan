b73e1439122dbe78a65958e566e11cd38a08892e
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2022-09-14 10:30:15 +0200

    fix(admin): remove legacy field

--- evan/site/admin/contents.py
+++ evan/site/admin/contents.py
@@ -6,9 +6,9 @@ from evan.models import Content
 @admin.register(Content)
 class ContentAdmin(admin.ModelAdmin):
     list_display = ("id", "event", "key")
-    list_filter = ("event",)
+    list_filter = (("event", admin.RelatedOnlyFieldListFilter),)
 
     def get_readonly_fields(self, request, obj=None):
         if not request.user.is_superuser:
-            return ("key", "notes")
+            return ("event", "key", "config")
         return ()
