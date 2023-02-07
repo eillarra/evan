399a7cb1c7cdec80bff4b513a7effa3d6de8309e
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2023-02-07 11:22:42 +0100

    chore: Django security update

--- evan/settings/development.py
+++ evan/settings/development.py
@@ -7,18 +7,24 @@ ALLOWED_HOSTS = ("localhost",)
 INTERNAL_IPS = ("127.0.0.1",)
 
 
+# https://django-debug-toolbar.readthedocs.io/en/stable/
+
 try:
     import debug_toolbar  # noqa
 
     INSTALLED_APPS += ("debug_toolbar",)  # noqa
     MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",)  # noqa
-except Exception:
+except ModuleNotFoundError:
     pass
 
 
 # https://docs.djangoproject.com/en/dev/topics/cache/
 
-CACHES = {"default": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache"}}
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+    }
+}
 
 CACHE_MIDDLEWARE_SECONDS = 20
 
