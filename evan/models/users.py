51bb896a599e9bf1cebaa60be9c62d6c68830f72
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2023-06-08 14:52:53 +0200

    feat: automatically link existing ugent accounts

--- evan/models/users.py
+++ evan/models/users.py
@@ -1,9 +1,14 @@
+from allauth.account.models import EmailAddress
+from allauth.socialaccount.signals import pre_social_login
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
 from django_countries.fields import CountryField
+
+
+User = get_user_model()
 
 
 class ProfileManager(models.Manager):
@@ -16,7 +21,7 @@ class Profile(models.Model):
     A user profile complements the base User model with extra information.
     """
 
-    user = models.OneToOneField(get_user_model(), primary_key=True, related_name="profile", on_delete=models.CASCADE)
+    user = models.OneToOneField(User, primary_key=True, related_name="profile", on_delete=models.CASCADE)
     position = models.CharField(max_length=190, null=True, blank=True)
     affiliation = models.CharField(max_length=190, null=True, blank=True)
     country = CountryField()
@@ -58,3 +63,26 @@ def post_save_user(sender, instance, created, **kwargs):
     else:
         instance.profile.updated_at = timezone.now()
         instance.profile.save()
+
+
+def find_user_by_email(email: str, verified: bool = True) -> User | None:
+    try:
+        return EmailAddress.objects.get(email__iexact=email, verified=verified).user
+    except EmailAddress.DoesNotExist:
+        return None
+
+
+@receiver(pre_social_login)
+def link_to_existing_user(sender, request, sociallogin, **kwargs):
+    if sociallogin.is_existing:
+        return
+
+    # for "ugent" social accounts, we can use the email address to find the user
+    if sociallogin.account.provider == "ugent":
+        try:
+            email = sociallogin.account.extra_data["mail"]
+            user = find_user_by_email(email)
+            if user:
+                sociallogin.connect(request, user)
+        except KeyError:
+            return
